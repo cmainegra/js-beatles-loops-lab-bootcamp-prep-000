488efbd7242c315657308e476c07316d425556cf
Charles Mainegra <cmainegra@gmail.com>
2018-01-03 19:36:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (let i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+   }
+   return beatles;
+}
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i <= facts.length) {
+  facts[i] = facts[i] + "!!!";
+  i++;
+    }
+  return facts;
+}
